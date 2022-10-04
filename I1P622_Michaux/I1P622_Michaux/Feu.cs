c5cb226b6912ee43074cd126e952ef50d34bfdfa
DMzartex <91669692+DMzartex@users.noreply.github.com>
2022-10-04 14:34:08 +0200

    Add files via upload

---
+++ I1P622_Michaux/I1P622_Michaux/Feu.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I1P622_Michaux
+{
+    class Feu
+    {
+        private string nomFeu;
+        private int couleurFeu;
+        private string etatFeu;
+
+        public Feu(string nomFeu, int couleurFeu,string etatFeu)
+        {
+          this.nomFeu = nomFeu;
+          this.couleurFeu = couleurFeu;
+          this.etatFeu = etatFeu;
+        }
+
+        public string afficheFeu()
+        {
+            if(couleurFeu == 1)
+            {
+                return "Le feu de signalisation " + nomFeu + " est vert";
+            }
+            else if (couleurFeu == 0)
+            {
+                return "Le feu de signalisation " + nomFeu + " est rouge";
+            }
+            else
+            {
+                return "Le feu de signalisation " + nomFeu + " est orange"; ;
+            }
+            
+        }
+
+        public void changeFeu()
+        {
+
+                if (this.couleurFeu == 1)
+                {
+                    this.couleurFeu = 2;
+                    etatFeu = "allumé";
+                   
+                }
+                else if (this.couleurFeu == 2)
+                {
+                    this.couleurFeu = 0;
+                    etatFeu = "éteint";    
+                }
+                else
+                {
+                    this.couleurFeu = 1;
+                    etatFeu = "éteint";
+                }
+        }
+
+        public string afficheEtatFeu()
+        {
+            return nomFeu + " est " + etatFeu;
+        }
+
+    }
+
+}
+
---
+++ I1P622_Michaux/I1P622_Michaux/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace I1P622_Michaux // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int continu = 1;
+
+            Feu feu_1 = new Feu("0010", 1, "éteint");
+            Feu feu_2 = new Feu("0011", 2, "allumé");
+
+            Console.WriteLine("Etat des feux :");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(feu_1.afficheFeu());
+            Console.WriteLine(feu_2.afficheFeu());
+
+            Console.WriteLine("Changement d'état :");
+            Console.WriteLine("-----------------------------");
+            
+            for(int n = 0; n < 7; n++)
+            {
+                feu_1.changeFeu();
+                Console.WriteLine(feu_1.afficheFeu());
+
+            }
+
+            Console.WriteLine("Feu clignotant :");
+            Console.WriteLine("----------------------------");
+
+            for (int n = 0; n < 7; n++)
+            {
+                feu_2.changeFeu();
+                Console.WriteLine(feu_2.afficheEtatFeu());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
